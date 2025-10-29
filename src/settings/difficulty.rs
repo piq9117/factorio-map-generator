86b6e434fa02aae5943c7d8481c87e0f00c0cd1c
piq9117 <piq9117@gmail.com>
2025-10-29 18:19:01 -0400

    deserialize difficulty settings

--- src/settings/difficulty.rs
+++ src/settings/difficulty.rs
@@ -1,5 +1,7 @@
+use serde::de::{IgnoredAny, MapAccess, Visitor};
 use serde::ser::SerializeStruct;
-use serde::{Serialize, Serializer};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::fmt;
 
 pub struct DifficultySettings {
     pub technology_price_multiplier: Option<f32>,
@@ -21,5 +23,46 @@ impl Serialize for DifficultySettings {
             &self.spoil_time_modifier.unwrap_or(0.0),
         )?;
         s.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for DifficultySettings {
+    fn deserialize<D>(deserializer: D) -> Result<DifficultySettings, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct DifficultySettingsVisitor;
+        impl<'de> Visitor<'de> for DifficultySettingsVisitor {
+            type Value = DifficultySettings;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("DifficultySettings")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> Result<Self::Value, V::Error>
+            where
+                V: MapAccess<'de>,
+            {
+                let mut technology_price_multiplier: Option<f32> = None;
+                let mut spoil_time_modifier: Option<f32> = None;
+
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        "technology_price_multiplier" => {
+                            technology_price_multiplier = Some(map.next_value()?)
+                        }
+                        "spoil_time_modifier" => spoil_time_modifier = Some(map.next_value()?),
+                        _ => {
+                            let _: IgnoredAny = map.next_value()?;
+                        }
+                    }
+                }
+                Ok(DifficultySettings {
+                    technology_price_multiplier: technology_price_multiplier,
+                    spoil_time_modifier: spoil_time_modifier,
+                })
+            }
+        }
+        deserializer.deserialize_map(DifficultySettingsVisitor)
     }
 }
