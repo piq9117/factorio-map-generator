09be6727d9b487b32e2a16f8b074905ecebc2bab
piq9117 <piq9117@gmail.com>
2025-10-29 12:29:26 -0400

    infer target directory

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use serde_json;
+use std::env;
 use std::fs::File;
 
 mod settings;
 use settings::default::mk_default_settings;
 
 fn main() -> std::io::Result<()> {
-    let file = File::create("map-settings.json")?;
+    let file_with_path = env::current_dir()?.join("map-settings.json");
+    let file = File::create(file_with_path)?;
     let map_settings = mk_default_settings();
 
     serde_json::to_writer(file, &map_settings)?;
